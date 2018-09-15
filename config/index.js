e7e1b618d4a21c79caf8500b98f8e831c5f8b1c8
michaelmanteo <michaelmanteo@gmail.com>
2018-09-15 13:59:27 -0400

    added config file

---
+++ config/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+    openCage_KEY: process.env.openCage_KEY
+}
--- server/service.js
+++ server/service.js
@@ -1,36 +1,29 @@
 'use strict';
 
+const config = require('../config');
 const express = require('express');
 const service = express();
 const request = require('superagent');
 const moment = require('moment');
 
+// https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=b48033fa43334d069d193fcb1fda9ec3
+let openCage_KEY= config.openCage_KEY; 
 
 service.get('/service/:location', (req, res, next) => {
-
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=GEO_API_KEY', (err, response) => {
+    
+    const PLACENAME = req.params.location;
+    request.get('https://api.opencagedata.com/geocode/v1/json?q='+PLACENAME + '&key='+openCage_KEY, (err, response)=>{
         if(err) {
             console.log(err);
             return res.sendStatus(500);
         }
+        var offset = response.body.results[0].annotations.timezone.offset_sec
 
-        const location = response.body.results[0].geometry.location;
-        const timestamp = +moment().format('X');
+        const timeStamp = +moment().format('X');
+        const timeString = moment.unix(timeStamp + offset).utc().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=TIMEZONE_API_KEY', (err, response) => {
-            if(err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            
-            const result = response.body;
-
-            const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
-        
-            res.json({result: timeString});
-        });
+        res.json({ result: timeString });
     });
-
 });
 
 module.exports = service;
