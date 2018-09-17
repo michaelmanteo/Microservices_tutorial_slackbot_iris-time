b0b2aafffb0cf87c7f4b5fbe930b952781400393
michaelmanteo <michaelmanteo@gmail.com>
2018-09-17 18:03:00 -0400

    replaced console.log()'s with bunyan logger

--- config/index.js
+++ config/index.js
@@ -1,5 +1,23 @@
 require('dotenv').config();
 
+const bunyan = require('bunyan');
+
+const log = {
+    development: () => {
+        return bunyan.createLogger({name: 'IRIS-Time-development', level: 'debug'});
+    },
+    production: () => {
+        return bunyan.createLogger({name: 'IRIS-Time-production', level: 'info'});
+    },
+    test: () => {
+        return bunyan.createLogger({name: 'IRIS-Time-test', level: 'fatal'});
+    }
+};
+
 module.exports = {
-    openCage_KEY: process.env.openCage_KEY
-}
+    openCage_KEY: process.env.openCage_KEY,
+    log: (env) => {
+        if(env) return log[env]();
+        return log[process.env.NODE_ENV || 'development']();
+    }
+};
--- server/service.js
+++ server/service.js
@@ -1,29 +1,30 @@
 'use strict';
 
-const config = require('../config');
 const express = require('express');
 const service = express();
 const request = require('superagent');
 const moment = require('moment');
 
-// https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=b48033fa43334d069d193fcb1fda9ec3
-let openCage_KEY= config.openCage_KEY; 
+module.exports = (config) => {
+    const log = config.log();
+    let openCage_KEY= config.openCage_KEY; 
 
-service.get('/service/:location', (req, res, next) => {
+    service.get('/service/:location', (req, res) => {
     
-    const PLACENAME = req.params.location;
-    request.get('https://api.opencagedata.com/geocode/v1/json?q='+PLACENAME + '&key='+openCage_KEY, (err, response)=>{
-        if(err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        var offset = response.body.results[0].annotations.timezone.offset_sec
-
-        const timeStamp = +moment().format('X');
-        const timeString = moment.unix(timeStamp + offset).utc().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-        res.json({ result: timeString });
+        const PLACENAME = req.params.location;
+        request.get('https://api.opencagedata.com/geocode/v1/json?q='+PLACENAME + '&key='+openCage_KEY, (err, response)=>{
+            if(err) {
+                log.error(err);
+                return res.sendStatus(500);
+            }
+            var offset = response.body.results[0].annotations.timezone.offset_sec
+    
+            const timeStamp = +moment().format('X');
+            const timeString = moment.unix(timeStamp + offset).utc().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    
+            res.json({ result: timeString });
+        });
     });
-});
 
-module.exports = service;
+    return service;
+};
